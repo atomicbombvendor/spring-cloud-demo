023db61e86ee35fd97a5f4b2cebae7dcb83ccc75
xiaobaili <atomicbombvendor@gmail.com>
2019-08-15 14:26:07 +0800

    添加eureka集群测试和 provider集群测试，ribbon负载均衡测试

--- consumer-test-80/src/main/java/com/test/consumer/controller/DeptController_Consumer.java
+++ consumer-test-80/src/main/java/com/test/consumer/controller/DeptController_Consumer.java
@@ -1,8 +1,12 @@
 package com.test.consumer.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.test.api.entity.Dept;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,5 +37,10 @@ public class DeptController_Consumer {
     @RequestMapping(value = "/consumer/dept/list")
     public List<Dept> list() {
         return restTemplate.getForObject(REST_URL_PREFIX + "/dept/list", List.class);
+    }
+
+    @RequestMapping(value = "/consumer/dept/discovery")
+    public Object discovery(){
+        return restTemplate.getForObject(REST_URL_PREFIX + "/detp/discovery", Object.class);
     }
 }
